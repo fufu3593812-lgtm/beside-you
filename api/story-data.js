04803593d6afd5ef4c8c99dd5cf3df9eaf39f10a
fufu3593812-lgtm <fufu3593812@gmail.com>
2026-07-24 19:15:45 +0800

    fix: story-data.js reads from JSON file, no template strings

--- api/story-data.js
+++ api/story-data.js
@@ -1,3 +1,4 @@
-// This file intentionally left as a stub.
-// Story data is now inlined in mcp.js to avoid Vercel require path issues.
-module.exports = {};
+var path = require('path');
+var data = JSON.parse(require('fs').readFileSync(path.join(__dirname, 'story-data.json'), 'utf8'));
+var CARD_STORIES = {"\u6492\u5a07":{rarity:"SSR\uff08\u9650\u5b9a\uff09",storyTitle:"Coquetry",story:"\u7c89\u96fe\u91cc\u5979\u662f\u4e00\u9897\u8fd8\u6ca1\u54ac\u4e0b\u53bb\u7684\u679c\u5b9e\u3002"},"\u751f\u6c14":{rarity:"SSR\uff08\u9650\u5b9a\uff09",storyTitle:"Wrath",story:"\u6709\u4e00\u5ea7\u56fe\u4e66\u9986\uff0c\u6bcf\u4e00\u672c\u4e66\u53ea\u6709\u4e00\u9875\u3002"}};
+module.exports = { STORIES: data, CARD_STORIES: CARD_STORIES };

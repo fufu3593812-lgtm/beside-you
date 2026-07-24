baa2dcd92a7e66e9d47eeff83bc230217d498c62
fufu3593812-lgtm <fufu3593812@gmail.com>
2026-07-25 00:31:10 +0800

    fix: remove unused serverless functions to stay under 12 limit

--- api/fix-touch.js
+++
@@ -1,4 +0,0 @@
-// This file patches sync-intimacy at backend level
-// It's included by overriding the sync-intimacy route in index.js
-// Actually we'll just update index.js directly
-module.exports = {};
